f8aa2cb7ff22ca42f0b943e685fca30561cae4b0
Alice Saito <saito.alice@mdpr.jp>
2020-05-26 17:41:36 +0900

    13回目

---
+++ SampleSource2020/Screens/RenshuAPI/RenshuAPITableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RenshuAPITableViewCell.swift
+//  SampleSource2020
+//
+//  Created by 斉藤アリス on 2020/05/26.
+//
+
+import UIKit
+
+class RenshuAPITableViewCell: UITableViewCell {
+    
+ 
+    @IBOutlet weak var iconImage: UIImageView!
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var techniqueLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
+
+
--- SampleSource2020/Screens/RenshuAPI/RenshuAPIViewController.swift
+++ SampleSource2020/Screens/RenshuAPI/RenshuAPIViewController.swift
@@ -6,18 +6,125 @@
 //
 
 import UIKit
+import Alamofire
+import Kingfisher
+
+struct Danin: Codable {
+    let id: String
+    let number: String
+    let name: String
+    let type: String
+    let techniques: [String]
+    let image_url: String
+}
 
 // ここでAPIの練習
 class RenshuAPIViewController: UIViewController {
-
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var daninInfo: [Danin] = []
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
+        fetch2()
     }
+    
+//    func fetch1() {
+//        /// 方法1　responseを取得する方法
+//        AF.request("https://5ecb6c2238df960016511fac.mockapi.io/ryodan/getDanin").responseJSON { (response) in
+//
+//            // jsonをData型を取得できる
+//            guard let data = response.data else {
+//                return
+//            }
+//
+//            do {
+//                // Data型のdataを、[Danin]でデコードする
+//                //encode：使いたい形式に変換
+//                //decode：形式を戻す
+//                let danins = try JSONDecoder().decode([Danin].self, from: data)
+//
+//                // この結果を確認してみて!
+//                print(danins, "❤️")
+//            } catch {
+//
+//                // 失敗したらこっちに入る（do catch try）
+//                print(error.localizedDescription)
+//            }
+//        }
+//    }
+
+    func fetch2() {
+        /// 方法2 デコードを一気にやってしまう
+        AF.request("https://5ecb6c2238df960016511fac.mockapi.io/ryodan/getDanin").responseDecodable(of: [Danin].self) { response in
+
+            // 成功したらrespnse.valueに[Danin]が入っている
+            //ココのvalueはkey:valueのvalueとは違う。
+            guard let danins = response.value else {
+                // 失敗した場合
+                print(response.error?.localizedDescription)
+                return
+            }
+
+            // この結果を確認してみて!
+            print(danins, "🐈")
+            dump(danins)
+        
+            
+            self.daninInfo = danins
+            //APIを使う時は必ずreloadする
+            self.tableView.reloadData()
+
+            self.indicator.stopAnimating() 
+            
+        }
+        
+        
+    }
+    
+    
+}
+
+extension RenshuAPIViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return daninInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RenshuAPITableViewCell") as! RenshuAPITableViewCell
+        
+        cell.iconImage.kf.setImage(with: URL(string: daninInfo[indexPath.row].image_url))
+        
+        cell.numberLabel.text = daninInfo[indexPath.row].number
+        cell.nameLabel.text = daninInfo[indexPath.row].name
+        cell.typeLabel.text = daninInfo[indexPath.row].type
+        cell.techniqueLabel.text = daninInfo[indexPath.row].techniques.joined(separator:"\n")
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
 
 
-// メモのフォルダに入ってるテキストをコピペ
+
+
+
+// メモのフォルダに入ってるテキストをコピペした
 
 /*
  APIを受信（get、fetch）するにはいくつか方法がある
@@ -28,7 +135,7 @@ class RenshuAPIViewController: UIViewController {
  今回のデータ
  https://5ecb6c2238df960016511fac.mockapi.io/ryodan/getDanin
 
- [] この仲はArray
+ [] この中はArray
  {} この中はDictionary
 
  DictionaryとなるまとまりをStructで定義する
@@ -46,7 +153,7 @@ class RenshuAPIViewController: UIViewController {
 
  ないかもしれないデータは　String?のようにOptionalにする
  keyと変数名は確実に同じようにする必要がある
- そうしないとエラーに成る
+ そうしないとエラーになる
  もしくは「この変数は、このkeyに対応する」というように書くことも出来る（省略）
 
 
@@ -60,7 +167,7 @@ class RenshuAPIViewController: UIViewController {
      }
 
      // ここにDaninの配列が入る
-     // これを用いてUITableViewに団員のデータを並べてみてください　課題2
+     // これを用いてUITableViewに団員のデータを並べてみてください
      dump(danins)
  }
 
@@ -134,7 +241,7 @@ class RenshuAPIViewController: UIViewController {
 
  // これだけ！！！
  
- import KingfisherSwiftUI
+ import Kingfisher
  
  func aaa() {
     let url = URL(string: "https://example.com/image.png")
