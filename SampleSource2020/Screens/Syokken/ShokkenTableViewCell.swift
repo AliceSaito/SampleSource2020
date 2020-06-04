ad7fea443e5a2f5e5b67556c30fcddfd5f73c887
Alice Saito <saito.alice@mdpr.jp>
2020-06-04 21:17:55 +0900

    enum4

---
+++ SampleSource2020/Screens/Syokken/ShokkenTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShokkenTableViewCell.swift
+//  SampleSource2020
+//
+//  Created by 斉藤アリス on 2020/06/04.
+//
+
+import UIKit
+
+class ShokkenTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+}
--- SampleSource2020/Screens/Syokken/ShokkenViewController.swift
+++ SampleSource2020/Screens/Syokken/ShokkenViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 // 要件
 // この店には、Aランチ、Bランチ、Cランチ、ラーメン、チャーハン、生姜焼き定食がある
-// 今、注文が文字列でordersで与えられている（食券機からデータが送られてきたっていう設定）
+// 今、注文が文字列ordersで与えられている（食券機からデータが送られてきたっていう設定）
 // "A-L"はAランチ "B-L"はBランチ　"C-L"はCランチ　"RAMEN"はラーメン　"FRIED RICE"はチャーハン "GINGER-T"は生姜焼き定食
 
 // 課題1. UITableViewを用いて、注文の一覧を表示する（例えば"A-L"の場合は、「Aランチ」と表示する）
@@ -22,7 +22,7 @@ import UIKit
 //       if文か、switch文でやる
 
 // 課題3-1. Aランチ、Bランチ、Cランチ、ラーメン、チャーハン、生姜焼き定食　を「enum Order」として定義する
-// 課題3-2. orders: [String]から、Orderに変換するfuncを作る
+// 課題3-2. order: Stringから、Orderに変換するfuncを作る
 // 課題3-3. enum Orderの中に、titleを返すfuncを作成し、cellを生成する時に使用する
 // 課題3-4. enum Orderの中に、priceを返すfuncを作成し、cellを生成する時に使用する
 
@@ -32,16 +32,137 @@ import UIKit
 //       enum Orderの中に、構成される一品一品を配列で返すdishesというfuncを作成してprintしてみる
 //       なお、ランチ以外、例えばラーメンの場合は「ラーメン」だけにする
 
-// 課題5. ordersの合計金額を求める
+// 課題5. ordersの合計金額を求める（for i in）
 
-// 課題6. なにか仕様を１つ追加して実装してみる
 
 class ShokkenViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     let orders: [String] = ["A-L", "B-L", "C-L", "RAMEN", "FRIED RICE", "GINGER-T", "A-L", "A-L", "B-L", "RAMEN", "GINGER-T", "C-L", "C-L", "RAMEN", "FRIED RICE", "RAMEN", "B-L", "C-L", "RAMEN", "FRIED RICE"]
+    
+
+    // A-Lなどから、Order.Aランチなどに変換
+    func henkan(order: String) -> Order {
+        if order == "A-L" {
+            return Order.Aランチ
+        }else if order == "B-L"{
+            return Order.Bランチ
+        }else if order == "C-L"{
+            return Order.Cランチ
+        }else if order == "RAMEN"{
+            return Order.ラーメン
+        }else if order == "FRIED RICE"{
+            return Order.チャーハン
+        }else {
+            return Order.生姜焼き定食
+        }
+    }
+    
+    enum Order: String {
+        case Aランチ
+        case Bランチ
+        case Cランチ
+        case ラーメン
+        case チャーハン
+        case 生姜焼き定食
+        
+        func getTitle() -> String{
+            switch self {
+            case .Aランチ:
+                return "Aランチ"
+            case .Bランチ:
+                return "Bランチ"
+            case .Cランチ:
+                return "Cランチ"
+            case .ラーメン:
+                return "ラーメン"
+            case .チャーハン:
+                return "チャーハン"
+            case .生姜焼き定食:
+                return "生姜焼き定食"
+            }
+        }
+        
+        func getPrice() -> Int{
+            switch self {
+            case .Aランチ:
+                return 550
+            case .Bランチ:
+                return 600
+            case .Cランチ:
+                return 850
+            case .ラーメン:
+                return 550
+            case .チャーハン:
+                return 450
+            case .生姜焼き定食:
+                return 1050
+            }
+        }
+        
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let v = henkan(order: orders[1])
+        print(v, "☀️")
     }
+}
 
+
+extension  ShokkenViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orders.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShokkenTableViewCell") as! ShokkenTableViewCell
+        
+//        cell.label.text = orders[indexPath.row]
+        
+//        if orders[indexPath.row] == "A-L" {
+//            cell.label.text = "Aランチ"
+//        }
+        
+        let orderString: String = orders[indexPath.row]     // orderStringは、例えば"A-L"
+        //↓ここで結びつけている！！！
+        let orderrrr: Order = henkan(order: orderString)      // orderは、例えばOrder.Aランチ
+        
+        cell.label.text = orderrrr.getTitle()
+        cell.priceLabel.text = "\(orderrrr.getPrice())" + "円"
+        //上のやつの長いバージョン
+//        switch order  {
+//        case "A-L":
+//            cell.label.text = "Aランチ"
+//            cell.priceLabel.text = "\(550)" + "円"
+//        case "B-L":
+//            cell.label.text = "Bランチ"
+//            cell.priceLabel.text = "\(600)" + "円"
+//        case "C-L":
+//            cell.label.text = "Cランチ"
+//            cell.priceLabel.text = "\(850)" + "円"
+//        case "RAMEN":
+//            cell.label.text = "ラーメン"
+//            cell.priceLabel.text = "\(550)" + "円"
+//        case "FRIED RICE":
+//            cell.label.text = "チャーハン"
+//            cell.priceLabel.text = "\(450)" + "円"
+//        case "GINGER-T":
+//            cell.label.text = "生姜焼き定食"
+//            cell.priceLabel.text = "\(1050)" + "円"
+//        default:
+//            cell.label.text = "なし"
+//            cell.priceLabel.text = "\(0)" + "円"
+//        }
+//
+        
+        return cell
+    }
 }
